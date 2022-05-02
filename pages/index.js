96d088fb52cb1cbdf6131e883859fac551fb0040
aznamle <namle1998@hotmail.com>
2022-05-02 16:50:11 -0700

    styling and components

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
 import React from 'react'
 
+import { Product, FooterBanner, HeroBanner } from '../components'
+
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
 
-      <div>
+      <div className='products-heading'>
         <h2>Best selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
-      <div>
+      <div className='products-container'>
         {['Product 1', 'Product 2'].map(
           (product) => product
         )}
       </div>
 
-      Footer
+      <FooterBanner />
     </>
   )
 }
