d2ebe767afb58e464d98e5c9e89331bec7493f33
aznamle <namle1998@hotmail.com>
2022-05-04 15:40:05 -0700

    cart structure

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -4,11 +4,15 @@ import { client, urlFor } from '../../lib/client';
 
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai'
 import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
     
     const { image, name, details, price } = product;
     const [ index, setIndex ] = useState(0);
+
+    //useContext hook
+    const { decQty, incQty, qty, onAdd } = useStateContext();
 
     return (
         <div>
@@ -43,13 +47,13 @@ const ProductDetails = ({ product, products }) => {
                     <div className='quantity'>
                         <h3>Quantity:</h3>
                         <p className='quantity-desc'>
-                            <span className='minus' onClick=''><AiOutlineMinus /></span>
-                            <span className='num' onClick=''>0</span>
-                            <span className='plus' onClick=''><AiOutlinePlus /></span>
+                            <span className='minus' onClick={decQty}><AiOutlineMinus /></span>
+                            <span className='num'>{qty}</span>
+                            <span className='plus' onClick={incQty}><AiOutlinePlus /></span>
                         </p>
                     </div>
                     <div className='buttons'>
-                        <button type='button' className='add-to-cart' onClick=''>Add to Cart</button>
+                        <button type='button' className='add-to-cart' onClick={() => onAdd(product,qty)}>Add to Cart</button>
                         <button type='button' className='buy-now' onClick=''>Buy Now</button>
                     </div>
                 </div>
