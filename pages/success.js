99bd30f0ead1362f139e6cc44afc6ce639bdfd9a
aznamle <namle1998@hotmail.com>
2022-05-05 21:31:37 -0700

    stripe integration finished, succes page finished

---
+++ pages/success.js
@@ -0,0 +1,41 @@
+import React, {useState, useEffect } from 'react'
+import { BSBagCheckFill } from 'react-icons/bs'
+
+import { useStateContext } from '../context/StateContext'
+import { runFireWorks } from '../lib/utils'
+
+const Success = () => {
+    const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+    const [order, setOrder] = useState(null);
+
+    useEffect(() => {
+        localStorage.clear();
+        setCartItems([])
+        setTotalPrice(0);
+        setTotalQuantities(0);
+        runFireWorks();
+    }, [])
+    
+    return (
+        <div className='success-wrapper'>
+            <div className='success'>
+                <p className='icon'>
+                    <BSBagCheckFill />
+                </p>
+                <h2>Thank you for your order!</h2>
+                <p className='email-msg'>Check your email inbox for the receipt.</p>
+                <p className='description'>
+                    If you have any questions, please email <a className='email' href='mailto:order@example.com'>order@example.com</a>
+                </p>
+                <Link href='/'>
+                    <button type='button' width='300px' className='btn'>
+                        Continue Shopping
+                    </button>
+                </Link>
+            </div>
+        </div>
+      )
+}
+
+export default Success
