26e4fa74066751b2b9f000aa476d3aa98a9ec9a6
aznamle <namle1998@hotmail.com>
2022-05-05 21:34:01 -0700

    buy now button onclick

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -12,7 +12,13 @@ const ProductDetails = ({ product, products }) => {
     const [ index, setIndex ] = useState(0);
 
     //useContext hook
-    const { decQty, incQty, qty, onAdd } = useStateContext();
+    const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+    const handleBuyNow = () => {
+        onAdd(product, qty);
+
+        setShowCart(true);
+    }
 
     return (
         <div>
@@ -54,7 +60,7 @@ const ProductDetails = ({ product, products }) => {
                     </div>
                     <div className='buttons'>
                         <button type='button' className='add-to-cart' onClick={() => onAdd(product,qty)}>Add to Cart</button>
-                        <button type='button' className='buy-now' onClick=''>Buy Now</button>
+                        <button type='button' className='buy-now' onClick={handleBuyNow}>Buy Now</button>
                     </div>
                 </div>
             </div>
